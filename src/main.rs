6680ad71d4eaf22343dd76dcad3107aebf8059f4
Dimitri Teboul <dimitri.teboul@viaco.fr>
2023-01-07 23:07:48 +0100

    remove dead code and fix missing file error

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,8 @@
-use std::error::Error;
-use std::fmt;
 use std::path::Path;
 use std::{collections::HashMap, env};
 
 use git_repository::{discover, objs::tree::EntryMode, traverse::tree::Recorder, Commit};
 use rust_code_analysis::{metrics, read_file_with_eol, ParserTrait, RustParser};
-
-#[derive(Debug)]
-struct MyError {}
-
-impl fmt::Display for MyError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", "WTF")
-    }
-}
-
-impl Error for MyError {
-    fn description(&self) -> &str {
-        "WTF"
-    }
-}
 
 #[derive(Debug, Clone)]
 struct FileMetrics {
@@ -29,10 +12,9 @@ struct FileMetrics {
 }
 
 fn main() {
-    // let opts = Opts::parse();
-    let mut current_dir = env::current_dir().expect("current dir");
+    let current_dir = env::current_dir().expect("current dir");
 
-    let repo = discover(current_dir.clone()).expect("repo");
+    let repo = discover(current_dir).expect("repo");
 
     let head = repo.head_commit().expect("head");
     let mut change_map = HashMap::new();
@@ -51,36 +33,37 @@ fn main() {
 
         for entry in recorder.records {
             if let &EntryMode::Blob = &entry.mode {
-                if change_map.contains_key(&entry.filepath) {
-                    change_map
-                        .entry(entry.filepath)
-                        .and_modify(|count| *count += 1);
-                } else {
-                    change_map.insert(entry.filepath, 1);
+                match change_map.entry(entry.filepath) {
+                    std::collections::hash_map::Entry::Occupied(mut e) => {
+                        *e.get_mut() += 1;
+                    }
+                    std::collections::hash_map::Entry::Vacant(e) => {
+                        e.insert(1);
+                    }
                 }
             }
         }
     }
-    current_dir.push("src");
 
     let results: Vec<FileMetrics> = change_map
         .into_iter()
-        .map(|(filename, churn)| {
+        .filter_map(|(filename, churn)| {
             let filename = filename.to_string();
             let path = Path::new(&filename);
 
-            let source = read_file_with_eol(path)
-                .expect("source")
-                .expect("option source");
+            let complexity = read_file_with_eol(path)
+                .ok()
+                .and_then(|file| {
+                    let parser = RustParser::new(file.expect("file content"), path, None);
+                    metrics(&parser, path)
+                })
+                .map(|metrics| metrics.metrics.cyclomatic.cyclomatic_sum());
 
-            let parser = RustParser::new(source, path, None);
-            let metrics = metrics(&parser, path).expect("metrics");
-
-            FileMetrics {
+            complexity.map(|complexity| FileMetrics {
                 churn,
                 filename: filename.to_string(),
-                complexity: metrics.metrics.cyclomatic.cyclomatic_sum(),
-            }
+                complexity,
+            })
         })
         .collect();
 
