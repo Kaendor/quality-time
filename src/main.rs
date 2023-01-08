f794d1410ef3b68cb696dd8a0cfaf7c9ce0e3b24
Dimitri Teboul <dimitri.teboul@viaco.fr>
2023-01-08 10:50:01 +0100

    ♻️ (metrics): move metric calculation into dedicated module

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
 use std::env;
-use std::path::Path;
 
 use comfy_table::modifiers::UTF8_ROUND_CORNERS;
 use comfy_table::presets::UTF8_FULL;
 use comfy_table::Table;
 use git_repository::discover;
-use rust_code_analysis::{metrics, read_file_with_eol, ParserTrait, RustParser};
 
 use crate::git::change_count_per_file;
-use crate::metrics::FileMetrics;
+use crate::metrics::metrics_per_file;
 
 mod git;
 mod metrics;
@@ -21,23 +19,7 @@ fn main() {
 
     let change_map = change_count_per_file(repo);
 
-    let results: Vec<FileMetrics> = change_map
-        .into_iter() // TODO: parallelize
-        .filter_map(|(filename, churn)| {
-            let filename = filename;
-            let path = Path::new(&filename);
-
-            let complexity = read_file_with_eol(path)
-                .ok()
-                .and_then(|file| {
-                    let parser = RustParser::new(file.expect("file content"), path, None);
-                    metrics(&parser, path)
-                })
-                .map(|metrics| metrics.metrics.cyclomatic.cyclomatic_sum());
-
-            complexity.map(|complexity| FileMetrics::new(filename.to_string(), churn, complexity))
-        })
-        .collect();
+    let results = metrics_per_file(change_map);
 
     let mut table = Table::new();
     table
--- src/metrics.rs
+++ src/metrics.rs
@@ -1,3 +1,7 @@
+use std::{collections::HashMap, path::Path};
+
+use rust_code_analysis::{metrics, read_file_with_eol, ParserTrait, RustParser};
+
 #[derive(Debug, Clone)]
 pub struct FileMetrics {
     pub filename: String,
@@ -13,4 +17,24 @@ impl FileMetrics {
             complexity,
         }
     }
+}
+
+pub fn metrics_per_file(file_map: HashMap<String, i32>) -> Vec<FileMetrics> {
+    file_map
+        .into_iter() // TODO: parallelize
+        .filter_map(|(filename, churn)| {
+            let filename = filename;
+            let path = Path::new(&filename);
+
+            let complexity = read_file_with_eol(path)
+                .ok()
+                .and_then(|file| {
+                    let parser = RustParser::new(file.expect("file content"), path, None);
+                    metrics(&parser, path)
+                })
+                .map(|metrics| metrics.metrics.cyclomatic.cyclomatic_sum());
+
+            complexity.map(|complexity| FileMetrics::new(filename.to_string(), churn, complexity))
+        })
+        .collect()
 }
