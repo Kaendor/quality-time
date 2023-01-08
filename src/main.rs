355c8141f3a3b138a40b2b992dfc4633a454015b
Dimitri Teboul <dimitri.teboul@viaco.fr>
2023-01-08 11:01:57 +0100

    ♻️ (output): move output code into dedicated module

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 use std::env;
 
-use comfy_table::modifiers::UTF8_ROUND_CORNERS;
-use comfy_table::presets::UTF8_FULL;
-use comfy_table::Table;
 use git_repository::discover;
 
 use crate::git::change_count_per_file;
 use crate::metrics::metrics_per_file;
+use crate::output::{print_output, OutputMode};
 
 mod git;
 mod metrics;
+mod output;
 
 // TODO: Use local error type instead of expects
 fn main() {
     let path_to_repo = env::current_dir().expect("current dir");
+    let output = OutputMode::StdOut;
 
     let repo = discover(path_to_repo).expect("repo");
 
@@ -21,19 +21,5 @@ fn main() {
 
     let results = metrics_per_file(change_map);
 
-    let mut table = Table::new();
-    table
-        .set_header(vec!["Filename", "Churn", "Complexity"])
-        .load_preset(UTF8_FULL)
-        .apply_modifier(UTF8_ROUND_CORNERS);
-
-    for metric in results.iter() {
-        table.add_row(vec![
-            &metric.filename,
-            &metric.churn.to_string(),
-            &metric.complexity.to_string(),
-        ]);
-    }
-
-    println!("{table}");
+    print_output(output, results);
 }
---
+++ src/output.rs
@@ -0,0 +1,30 @@
+use crate::metrics::FileMetrics;
+use comfy_table::modifiers::UTF8_ROUND_CORNERS;
+use comfy_table::presets::UTF8_FULL;
+use comfy_table::Table;
+
+pub enum OutputMode {
+    StdOut,
+}
+
+pub fn print_output(output_mode: OutputMode, metrics: Vec<FileMetrics>) {
+    match output_mode {
+        OutputMode::StdOut => {
+            let mut table = Table::new();
+            table
+                .set_header(vec!["Filename", "Churn", "Complexity"])
+                .load_preset(UTF8_FULL)
+                .apply_modifier(UTF8_ROUND_CORNERS);
+
+            for metric in metrics.iter() {
+                table.add_row(vec![
+                    &metric.filename,
+                    &metric.churn.to_string(),
+                    &metric.complexity.to_string(),
+                ]);
+            }
+
+            println!("{table}");
+        }
+    }
+}
