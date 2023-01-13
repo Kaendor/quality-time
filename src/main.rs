f42a581808fcd86e5d403a2a809a56f30d72df30
Dimitri Teboul <dimitri.teboul@viaco.fr>
2023-01-13 18:37:55 +0100

    🥅 (error): use eyre to handle errors

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use eyre::{Context, Result};
 use std::env;
 
 use crate::git::change_count_in_path;
@@ -18,15 +19,18 @@ struct Args {
     output: Option<OutputMode>,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args = Args::parse();
 
     let path_to_repo = env::current_dir().expect("current dir");
     let output = args.output.unwrap_or(OutputMode::StdOut);
 
-    let change_map = change_count_in_path(path_to_repo);
+    let change_map = change_count_in_path(path_to_repo)
+        .wrap_err("Unable to obtain the change count per file")?;
 
     let results = metrics_per_file(change_map);
 
     print_output(output, results);
+
+    Ok(())
 }
